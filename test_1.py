1d90d5ef627efb4643a5cef69bc527fee3a4ce5e
root <root@localhost.localdomain>
2015-10-23 00:41:27 -0400

    My firist commit

---
+++ test_1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import Conf
+import main
+import sys
+
+
+if __name__ == "__main__":
+   config=Conf.conf("./conf.info")
+   m=main.mainClass(config)
+   m.makeConnectionToReg(config)
+   m.updateRegionsList(config,"")
+   print m.connToReg['us-west-2'].insts
+   m.CreateActiveInstListOfAllRegions()
+   print "The active list is %s" % str(m.runningInstList)
+   m.dumpAwsResourceInfo()
+   m.updateRegionsOtherResource(config,"")
+   for i in m.connToReg['us-west-2'].insts[16] :
+       i.actionWithInInst()
+   m.dumpExceptionList("./abc")
+   m.dumpResultSet()
+       
